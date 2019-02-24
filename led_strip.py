b38d533080534c06dde6bde73df40af7171eed28
Richard Kirby <richard.james.kirby@gmail.com>
2019-02-24 17:10:20 +0000

    Update to allow check of correct wiring.

--- led_strip.py
+++ led_strip.py
@@ -2,23 +2,53 @@ import pigpio
 import time
 import colorsys
 
-
+# Set up the pigpio library.
 pi = pigpio.pi()
 
+# Define the pins.
 green_pin =16
 blue_pin = 21
 red_pin =20
 
-low_pwm =50
-high_pwm =255
-
+# Turn off all the pins
 pi.set_PWM_dutycycle(green_pin, 0)
-
 pi.set_PWM_dutycycle(blue_pin, 0)
 pi.set_PWM_dutycycle(red_pin, 0)
 
 try:
+
+    print("Press Ctrl-C to finish")
+
+    # Set each pin to max in turn - make sure connected correctly
+    # Turn green on 100% for a couple of seconds and the off,
+    pi.set_PWM_dutycycle(green_pin, 255)
+    print("Green? 100%")
+    time.sleep(3)
+    pi.set_PWM_dutycycle(green_pin, int(255/10))
+    print("Green? 10%")
+    time.sleep(3)
+    pi.set_PWM_dutycycle(green_pin, 0)
+
+    pi.set_PWM_dutycycle(red_pin, 255)
+    print("Red?")
+    time.sleep(3)
+    pi.set_PWM_dutycycle(red_pin, int(255/10))
+    print("Red? 10%")
+    time.sleep(3)
+    pi.set_PWM_dutycycle(red_pin, 0)
+
+    pi.set_PWM_dutycycle(blue_pin, 255)
+    print("Blue? 100%")
+    time.sleep(3)
+    pi.set_PWM_dutycycle(blue_pin, int(255)/10)
+    print("Blue? 10%")
+    time.sleep(3)
+    pi.set_PWM_dutycycle(blue_pin, 0)
+
+    # Keep cycling through various colours - assume the user will press Control-C to finish.
     while True:
+
+        # Cycle through the top ends of value and saturation and through the different colours
         for value in range (75, 100):
 
             for saturation in range (75,100):
@@ -33,7 +63,12 @@ try:
 
                     time.sleep(0.01)
 
+except KeyboardInterrupt:
+    print("Control-C received")
+
+# Clean up to finish -important to turn pins off.
 finally:
     pi.set_PWM_dutycycle(green_pin, 0)
     pi.set_PWM_dutycycle(blue_pin, 0)
     pi.set_PWM_dutycycle(red_pin, 0)
+    pi.stop()
